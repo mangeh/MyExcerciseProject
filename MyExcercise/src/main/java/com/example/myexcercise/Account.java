c342e31767049f697bcabf640f037a87d4e55580
Magnus Hernegren <mangehl@gmail.com>
2013-06-11 14:35:33 +0200

    First

--- MyExcercise/src/main/java/com/example/myexcercise/Account.java
+++ MyExcercise/src/main/java/com/example/myexcercise/Account.java
@@ -4,4 +4,8 @@ package com.example.myexcercise;
  * Created by Magnus on 2013-06-11.
  */
 public class Account {
+    //Hello
+    private void HelloMethod(){
+    System.out.print("Hello");
+    }
 }
