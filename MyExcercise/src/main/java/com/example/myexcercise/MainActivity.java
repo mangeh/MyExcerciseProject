e09061ce800e30520b2671c47143cdeb0175e8c8
Magnus Hernegren <mangehl@gmail.com>
2013-06-11 21:26:37 +0200

    Cleaned up Excercise 1

--- MyExcercise/src/main/java/com/example/myexcercise/MainActivity.java
+++ MyExcercise/src/main/java/com/example/myexcercise/MainActivity.java
@@ -1,22 +1,25 @@
 package com.example.myexcercise;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         BankAccount bac = new BankAccount();
         bac.setAccountNumber(1234);
         System.out.println("bac = " + bac.getAccountNumber());
-        Account b = (BankAccount) bac;
+        Account b = bac;
         System.out.println("MainActivity.onCreate");
         System.out.println("b = " + b.getAccountNumber());
-
     }
 
 
